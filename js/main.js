af6994eeed8181a0822f302b5bb29969079b05ac
stevendev <stevendotpulido@gmail.com>
2018-09-13 19:59:44 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const submitBtn = document.querySelector('#submit');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+
+// Init Voices Array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  // Loop through voices and create an option for each one
+  voices.forEach(voice => {
+  // Create option element
+  const option = document.createElement('option');
+  // fill option with voice and language
+  option.textContent = voice.name + '('+voice.lang+')';
+  // Set needed option attributes
+  option.setAttribute('data-lang', voice.lang);
+  option.setAttribute('data-name', voice.name);
+  voiceSelect.appendChild(option);
+  });
+}
+
+getVoices();
+if(synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+// Speak
+const speak = () => {
+// Check if speaking
+  if(synth.speaking) {
+    console.error('Already speaking...');
+    return;
+  }
+  if(textInput.value !== '') {
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    // Speak end
+    speakText.onend = e => {
+      console.log('Done speaking...');
+    }
+    // Speak error
+    speakText.onerror = e => {
+      console.error('Something went wrong')
+    }
+    // Selected Voice
+    const selectedVoice = voiceSelect.selectedOptions[0]
+      .getAttribute('data-name');
+
+    // Loop through voices onselect
+    voices.forEach(voice => {
+      if(voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    // Speak
+    synth.speak(speakText);
+  }
+}
+
+// Event Listeners
+// Text form submit
+submitBtn.addEventListener('click', e => {
+  console.log('submit');
+
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// Rate Value Change
+rate.addEventListener('change', e => rateValue.textContent = rate.value);
+
+// Pitch Value Change
+pitch.addEventListener('change', e => pitchValue.textContent = pitch.value);
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
